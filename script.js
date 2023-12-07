794afd8377edbb6d267da2e28b5f957dc57fe23e
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2023-12-07 19:12:15 +0530

    Update style.css and script.js

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-// <!-- Quiz question start -->
-//   Question <!-- number --> of 10
-//   A
-//   B
-//   C
-//   D
-//   Submit answer
-//   <!-- Quiz question end -->
-//   <!-- Quiz completed start -->
-//   Quiz completed
-//   You scored...
-//   <!-- score --> out of 10
-//   <!-- Quiz completed end -->
+const quizTopic = document.getElementById("quiz-topic");
+const htmlTopic = document.getElementById("html");
+const cssTopic = document.getElementById("css");
+const jsTopic = document.getElementById("javascript");
+const accessTopic = document.getElementById("accessibility");
+const questionSection = document.getElementById("question-section");
+const answerSection = document.getElementById("answer-section");
+const submitBtn = document.getElementById("submit-btn");
+
+//Functions
+async function quizData() {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  console.log(data);
+}
+let quiz = quizData();
+
+function pageUpdate(user) {
+  quizTopic.innerHTML = `
+  <img src="assets/images/icon-${user}.svg" alt="${user}" />
+  <h3>HTML</h3>
+  `;
+
+  questionSection.innerHTML = `
+  <p>Question number</p>
+  <h2>Blah blaldl ahahahgduia ahsdhsdsidf</h2>
+  <div class='progress-bar'>
+  <div class='progress'></div>
+  </div>
+  `;
+  answerSection.innerHTML = `
+    <button id="option1"> <span class="option" >A</span> Answer 1 </button>
+    <button id="option2"> <span class="option" >B</span> Answer 2 </button>
+    <button id="option3"> <span class="option" >C</span> Answer 3 </button>
+    <button id="option4"> <span class="option" >D</span> Answer 4 </button>
+    <button class="answer-btn submit-btn" id="submit-btn">
+        Submit Answer
+    </button>
+  `;
+}
+// htmlTopic.addEventListener("click");
